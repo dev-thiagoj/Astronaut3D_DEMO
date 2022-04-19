62e30afe1e1c955f1dcb184c7deb2bf2398acc8d
Dev.ThiagoJ <dev.thiagoj@gmail.com>
2022-04-19 16:12:55 -0300

    [build] add state machine

---
+++ Assets/Scripts/Ebac/StateMachine/StateBase.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class StateBase
+{
+    public virtual void OnStateEnter(object o = null)
+    {
+        Debug.Log("On " + this + " Enter");
+    }
+
+    public virtual void OnStateStay()
+    {
+        Debug.Log("On " + this + " Enter");
+    }
+
+    public virtual void OnStateExit()
+    {
+        Debug.Log("On " + this + " Enter");
+    }
+}

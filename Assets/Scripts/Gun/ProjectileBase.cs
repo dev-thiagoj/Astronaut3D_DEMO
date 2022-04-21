360f5af7086a8adf587bc21b2c6fcd2f5c029acf
Dev.ThiagoJ <dev.thiagoj@gmail.com>
2022-04-21 11:28:54 -0300

    [build] add gun base

---
+++ Assets/Scripts/Gun/ProjectileBase.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileBase : MonoBehaviour
+{
+    public float timeToDestroy = 1f;
+
+    public int damageAmount = 1;
+    public float speed = 50;
+
+    private void Awake()
+    {   
+        Destroy(gameObject, timeToDestroy);
+    }
+
+    void Update()
+    {
+        transform.Translate(speed * Time.deltaTime * Vector3.forward);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}

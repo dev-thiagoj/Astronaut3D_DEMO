9c35f8f39b52fe47070a24c164c350d3319e6e4d
Dev.ThiagoJ <dev.thiagoj@gmail.com>
2022-04-21 08:40:26 -0300

    [build] add player run

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -12,6 +12,10 @@ public class Player : MonoBehaviour
     public float gravity = 9.8f;
     public float jumpSpeed = 15f;
 
+    [Header("Run Setup")]
+    public KeyCode keyRun = KeyCode.LeftShift;
+    public float speedRun = 1.5f;
+
     private float vSpeed = 0f;
 
     private void Update()
@@ -19,7 +23,7 @@ public class Player : MonoBehaviour
         transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0);
 
         var inputAxisVertical = Input.GetAxis("Vertical");
-        var speedVector = transform.forward * inputAxisVertical * speed;
+        var speedVector = inputAxisVertical * speed * transform.forward;
 
         if (characterController.isGrounded)
         {
@@ -27,6 +31,16 @@ public class Player : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.Space)) vSpeed = jumpSpeed;
         }
 
+        var isWalking = inputAxisVertical != 0;
+        if (isWalking)
+        {
+            if (Input.GetKey(keyRun))
+            {
+                speedVector *= speedRun;
+                animator.speed = speedRun;
+            }
+            else animator.speed = 1;
+        }
 
         vSpeed -= gravity * Time.deltaTime;
         speedVector.y = vSpeed;
@@ -37,6 +51,7 @@ public class Player : MonoBehaviour
         //else animator.SetBool("Run", false);
         //por ser um if/else bem simples, podemos fazer da seguinte forma com o mesmo resultado
 
-        animator.SetBool("Run", inputAxisVertical != 0);
+        animator.SetBool("Run", isWalking);
+
     }
 }

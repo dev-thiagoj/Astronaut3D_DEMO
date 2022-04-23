0440963678849fdca023e7923c0649cb4464557f
Dev.ThiagoJ <dev.thiagoj@gmail.com>
2022-04-23 09:30:21 -0300

    Revert "[fix] i dont know how to fix"
    This reverts commit 14477bdcf3f4865cdd18730647f0de285fbf7b03.

--- Assets/Scripts/Gun/GunBase.cs
+++ Assets/Scripts/Gun/GunBase.cs
@@ -5,10 +5,9 @@ using UnityEngine;
 public class GunBase : MonoBehaviour
 {
     public ProjectileBase prefabProjectile;
-
-    public Vector3 position;
     
     public Transform positionToShoot;
+    public Vector3 pos;
     public float timeBetweenShoot = .3f;
     public float speed = 50f;
 
@@ -16,8 +15,7 @@ public class GunBase : MonoBehaviour
 
     private void Update()
     {
-        position = positionToShoot.position;
-        position = positionToShoot.transform.position;
+        pos = positionToShoot.position;
     }
 
     protected virtual IEnumerator ShootCoroutine()
@@ -32,10 +30,8 @@ public class GunBase : MonoBehaviour
     protected virtual void Shoot()
     {   
         var projectile = Instantiate(prefabProjectile);
-        projectile.transform.SetPositionAndRotation(position, positionToShoot.rotation);
+        projectile.transform.SetPositionAndRotation(pos, positionToShoot.rotation);
         projectile.speed = speed;
-        projectile.transform.SetPositionAndRotation(position, positionToShoot.rotation);
-        
     }
 
     public void StartShoot()

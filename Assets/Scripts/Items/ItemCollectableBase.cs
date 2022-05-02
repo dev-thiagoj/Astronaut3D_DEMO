9ff26086491aa931dc80f0315aabfd1b1c9147c7
Dev.ThiagoJ <dev.thiagoj@gmail.com>
2022-05-02 15:11:04 -0300

    [build] add item collectable lifepack

--- Assets/Scripts/Items/ItemCollectableBase.cs
+++ Assets/Scripts/Items/ItemCollectableBase.cs
@@ -2,50 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemCollectableBase : MonoBehaviour
+
+namespace Itens
 {
-
-    public string compareTag = "Player";
-    public ParticleSystem particleSystem;
-    public GameObject graphicItem;
-    public float timeToHide = 0.1f;
-
-    [Header("Sounds")]
-    public AudioSource audioSource;
-
-
-    private void Awake()
+    public class ItemCollectableBase : MonoBehaviour
     {
+        public ItemType itemType;
 
-    }
+        public string compareTag = "Player";
+        public ParticleSystem particleSystem;
+        public GameObject graphicItem;
+        public float timeToHide = 0.1f;
 
-    private void OnTriggerEnter(Collider collision)
-    {
-        if (collision.transform.CompareTag(compareTag))
+        public Collider collider;
+
+        [Header("Sounds")]
+        public AudioSource audioSource;
+
+        private void OnValidate()
         {
-            Collect();
+            collider = GetComponent<Collider>();
+        }
+
+        private void Awake()
+        {
+            OnValidate();
+        }
+
+        private void OnTriggerEnter(Collider collision)
+        {
+            if (collision.transform.CompareTag(compareTag))
+            {
+                Collect();
+            }
+        }
+
+        protected virtual void Collect()
+        {
+            if (collider != null) collider.enabled = false;
+            if (graphicItem != null) graphicItem.SetActive(false);
+            Invoke(nameof(HideObject), timeToHide);
+            OnCollect();
+            //Destroy(gameObject, timeToDestroy);
+        }
+
+        private void HideObject()
+        {
+            gameObject.SetActive(false);
+        }
+
+        protected virtual void OnCollect()
+        {
+            //VFXManager.Instance.PlayVFXByType(VFXManager.VFXType.COLLECTCOINS, transform.position);
+            //if (audioSource != null) audioSource.Play();
+            //if (particleSystem != null) particleSystem.Play();
+
+            ItemManager.Instance.AddByType(itemType);
         }
     }
 
-    protected virtual void Collect()
-    {
-        if (graphicItem != null) graphicItem.SetActive(false);
-        Invoke(nameof(HideObject), timeToHide);
-        OnCollect();
-        //Destroy(gameObject, timeToDestroy);
-    }
-
-    private void HideObject()
-    {
-        gameObject.SetActive(false);
-    }
-
-    protected virtual void OnCollect()
-    {
-        //VFXManager.Instance.PlayVFXByType(VFXManager.VFXType.COLLECTCOINS, transform.position);
-        //if (audioSource != null) audioSource.Play();
-        //if (particleSystem != null) particleSystem.Play();
-
-
-    }
 }

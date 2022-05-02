c0fcd9cbb55d6a0935e15d03ca2a792b4af2970e
Dev.ThiagoJ <dev.thiagoj@gmail.com>
2022-05-02 10:30:32 -0300

    [refactor] boss trigger and more camera setup

--- Assets/Scripts/Boss/BossTrigger.cs
+++ Assets/Scripts/Boss/BossTrigger.cs
@@ -6,19 +6,40 @@ using Boss;
 public class BossTrigger : MonoBehaviour
 {
     public BossBase bossBase;
+    public GameObject bossCamera;
+    public Color gizmoColor = Color.yellow;
 
     private void OnValidate()
     {
         bossBase = GameObject.FindObjectOfType<BossBase>();
     }
 
+    private void Awake()
+    {
+        OnValidate();
+        bossCamera.SetActive(false);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            TurnBossCameraOn();
+
             bossBase.lookAtPlayer = true;
             bossBase.SwitchState(BossAction.WALK);
             gameObject.SetActive(false);
         }
+    }
+
+    private void TurnBossCameraOn()
+    {
+        bossCamera.SetActive(true);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = gizmoColor;
+        Gizmos.DrawSphere(transform.position, transform.localScale.y);
     }
 }
